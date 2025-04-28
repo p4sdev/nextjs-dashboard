ce55b3198af801ba610ef6ea028e5a3af551aaac
Vladimir Pashkin <vldmrpashkin@icloud.com>
2025-04-28 21:23:31 +0300

    ending chapter 10

--- app/dashboard/(overwie)/page.tsx
+++ app/dashboard/(overwie)/page.tsx
@@ -1,41 +1,42 @@
+import CardWrapper from '@/app/ui/dashboard/cards'
 import { Card } from '@/app/ui/dashboard/cards'
 import RevenueChart from '@/app/ui/dashboard/revenue-chart'
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices'
 import { lusitana } from '@/app/ui/fonts'
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data'
+import { fetchCardData } from '@/app/lib/data'
 import { Suspense } from 'react'
-import { RevenueChartSkeleton } from '@/app/ui/skeletons'
+import {
+	RevenueChartSkeleton,
+	LatestInvoicesSkeleton,
+	CardsSkeleton,
+} from '@/app/ui/skeletons'
 import { formatCurrency } from '@/app/lib/utils'
 
 export default async function Page() {
-	const latestInvoices = await fetchLatestInvoices()
 	const {
 		numberOfInvoices,
 		numberOfCustomers,
 		totalPaidInvoices,
 		totalPendingInvoices,
 	} = await fetchCardData()
-	
+
 	return (
 		<main>
 			<h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
 				Основная информация
 			</h1>
 			<div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
-				<Card title='Получено:' value={formatCurrency(totalPaidInvoices)} type='collected' />
-				<Card title='В ожидании:' value={formatCurrency(totalPendingInvoices)} type='pending' />
-				<Card title='Всего заказов:' value={numberOfInvoices} type='invoices' />
-				<Card
-					title='Всего пользователей:'
-					value={numberOfCustomers}
-					type='customers'
-				/>
+				<Suspense fallback={<CardsSkeleton />}>
+					<CardWrapper />
+				</Suspense>
 			</div>
 			<div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
 				<Suspense fallback={<RevenueChartSkeleton />}>
 					<RevenueChart />
 				</Suspense>
-				<LatestInvoices latestInvoices={latestInvoices} />
+				<Suspense fallback={<LatestInvoicesSkeleton />}>
+					<LatestInvoices />
+				</Suspense>
 			</div>
 		</main>
 	)
