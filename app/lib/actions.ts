e12d42658994b7d130655906aa79782f40a196f3
Vladimir Pashkin <vldmrpashkin@icloud.com>
2025-05-04 00:43:53 +0300

    add Handling Errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,7 @@
 import { z } from 'zod'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-import postgres from 'postgres';
+import postgres from 'postgres'
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
@@ -27,10 +27,15 @@ export async function createInvoice(formData: FormData) {
 	const amountInCents = amount
 	const date = new Date().toISOString().split('T')[0]
 
-	await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `
+	try {
+		await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `
+	} catch (error) {
+		// We'll log the error to the console for now
+		console.error(error)
+	}
 	revalidatePath('/dashboard/invoices')
 	redirect('/dashboard/invoices')
 }
@@ -44,11 +49,16 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 	const amountInCents = amount
 
-	await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `
+	try {
+		await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `
+	} catch (error) {
+		// We'll log the error to the console for now
+		console.error(error)
+	}
 
 	revalidatePath('/dashboard/invoices')
 	redirect('/dashboard/invoices')
