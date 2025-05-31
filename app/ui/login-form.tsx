6945cf8afe4f0c09794a0444e9f4e1cfd643f0d9
Vladimir Pashkin <vldmrpashkin@icloud.com>
2025-05-31 09:39:56 +0300

    edit README

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -23,9 +23,9 @@ export default function LoginForm() {
 	return (
 		<form action={formAction} className='space-y-3'>
 			<div className='flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8'>
-				<h1 className={`${lusitana.className} mb-3 text-2xl`}>
-					Пожалуйста авторизуйтесь
-				</h1>
+				{/* <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+					Пожалуйста введите данные
+				</h1> */}
 				<div className='w-full'>
 					<div>
 						<label
