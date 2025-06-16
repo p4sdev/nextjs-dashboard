e4d363d0dcc502c3d0d0f92ff3db37ad20b3dc39
Vladimir Pashkin <vldmrpashkin@icloud.com>
2025-06-16 12:18:44 +0300

    add: animation, fix: logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts'
+import AcmeLogo from '@/app/ui/logo'
+import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Page() {
-  return (
+	return (
 		<main className='flex min-h-screen flex-col p-6'>
 			<div className='flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52'>
 				<AcmeLogo />
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -28,7 +28,7 @@ const links = [
 export default function NavLinks() {
 	const pathname = usePathname()
 	return (
-		<>
+		<div className='flex flex-col gap-2'>
 			{links.map(link => {
 				const LinkIcon = link.icon
 				return (
@@ -36,7 +36,7 @@ export default function NavLinks() {
 						key={link.name}
 						href={link.href}
 						className={clsx(
-							'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+							'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
 							{
 								'bg-sky-100 text-blue-600': pathname === link.href,
 							}
@@ -47,6 +47,6 @@ export default function NavLinks() {
 					</Link>
 				)
 			})}
-		</>
+		</div>
 	)
 }
