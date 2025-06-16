c0d46d9bcf34b1786a6dd5127709740f44dda8de
Vladimir Pashkin <vldmrpashkin@icloud.com>
2025-06-16 04:23:47 +0300

    fix: deploy errors

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,11 @@
 import CustomersTable from '@/app/ui/customers/table'
 import { fetchFilteredCustomers } from '@/app/lib/data'
+import { Suspense } from 'react'
 
 export default async function Page() {
 	const customers = await fetchFilteredCustomers('')
 
 	const table = await CustomersTable({ customers })
 
-	return <div>{table}</div>
+	return <Suspense>{table}</Suspense>
 }
